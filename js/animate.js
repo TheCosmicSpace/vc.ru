fb4ccb48b6b612ba362ea45e623974de0a4177cf
TheAdminPro <45514836+TheAdminPro@users.noreply.github.com>
2020-06-14 21:37:28 +0300

    animate api added

---
+++ js/animate.js
@@ -0,0 +1,25 @@
+const speed = 350;
+
+export function fadeIn(element, isMove = true){
+  const seconds = speed / 1000;
+  element.style.transition = `${seconds}s ease-in-out`;
+  element.style.opacity = 1;
+  if(isMove) element.style.transform = 'translateX(0px)';
+}
+
+export function fadeOut(element, callback = null, isMove = true){
+  const seconds = speed / 1000;
+  element.style.transition = `${seconds}s ease-in-out`;
+  element.style.opacity = 0;
+  if(isMove) element.style.transform = 'translateX(30px)';
+  
+  setTimeout(function() {
+      if(callback) callback({element, style: element.style});
+  }, speed);
+}
+export function flash(element, callback = null, isMove = true){
+  fadeOut(element, () => {
+    if(callback) callback({element, style: element.style});
+    fadeIn(element, isMove);
+  }, isMove);
+}
